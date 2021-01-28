c88cc4ef8bf356df8a845c96f10474c6d7cfb9f8
Mark Miranda <notmarkmiranda@gmail.com>
2021-01-27 21:46:12 -0700

    Start GameDetailScoring layout

--- KBReffing/GameDetailScoring.swift
+++ KBReffing/GameDetailScoring.swift
@@ -11,13 +11,50 @@ struct GameDetailScoring: View {
     @State var game: Game!  
     
     var body: some View {
-        HStack {
-            Text(game.awayTeamName)
-                .accessibility(identifier: "awayTeamName")
-            Text("vs.")
-            Text(game.homeTeamName)
-                .accessibility(identifier: "homeTeamName")
+        VStack(alignment: .leading) {
+            Text(game.formattedDate())
+                .fontWeight(.light)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            HStack(alignment: .top) {
+                VStack {
+                    Text(game.awayTeamName.uppercased())
+                        .bold()
+                        .accessibility(identifier: "awayTeamName")
+                        .padding(.bottom, 10)
+                    Text("9").font(.largeTitle).fontWeight(.black)
+                }
+                .padding(.horizontal, 2)
+                .frame(maxWidth: .infinity)
+                
+                VStack {
+                    Text("vs.")
+                }
+                
+                VStack {
+                    Text(game.homeTeamName.uppercased())
+                        .bold()
+                        .accessibility(identifier: "homeTeamName")
+                        .padding(.bottom, 10)
+                    Text("2").font(.largeTitle).fontWeight(.black)
+                }
+                .padding(.horizontal, 2)
+                .frame(maxWidth: .infinity)
+                
+            }
+            .padding(.top, 4)
+            
+            Spacer()
         }
+        .frame(
+            minWidth: 0,
+            maxWidth: .infinity,
+            minHeight: 0,
+            maxHeight: .infinity,
+            alignment: .topLeading
+        )
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        .navigationBarTitle("", displayMode: .inline)
+        .padding()
     }
 }
 
